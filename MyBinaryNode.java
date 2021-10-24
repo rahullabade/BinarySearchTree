d01397422c312765a98418e07dbecd4f4d1183c5
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-24 23:13:08 +0530

    Added: UC1_BinaryTree

---
+++ MyBinaryNode.java
@@ -0,0 +1,18 @@
+package binarySearchTree;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+	
+	public MyBinaryNode(K key) {
+		this.key = key;
+		left = null;
+		right = null;
+	}
+
+	@Override
+	public String toString() {
+		return "MyBinaryNode [key=" + key + ", left=" + left + ", right=" + right + "]";
+	}
+}
